e4f93df2b8c763c27b7b5bf0e98adeda3fa65e8b
Patryk Kwaśniok <kwasniokpatryk@gmail.com>
2024-09-09 19:38:35 +0200

    Added basic OS info fetching

--- src/main.zig
+++ src/main.zig
@@ -41,17 +41,24 @@ pub fn main() !void {
             var memory_info = sysfetch.MemoryInfo.init(allocator);
             defer memory_info.deinit();
 
+            var os_info = sysfetch.OSInfo.init(allocator);
+            defer os_info.deinit();
+
             try cpu_info.fetch();
             try memory_info.fetch();
+            try os_info.fetch();
 
             try stdout.print("CPU\n", .{});
-            try stdout.print(" ├─ Manufacturer: {?s}\n", .{cpu_info.manufacturer_name});
-            try stdout.print(" ├─ Model: {?s}\n", .{cpu_info.model_name});
-            try stdout.print(" └─ No. of cores: {?d}\n", .{cpu_info.cores});
-            try stdout.print("    └─ No. of threads: {?d}\n", .{cpu_info.threads});
+            try stdout.print("├─Manufacturer: {?s}\n", .{cpu_info.manufacturer_name});
+            try stdout.print("├─Model: {?s}\n", .{cpu_info.model_name});
+            try stdout.print("└─No. of cores: {?d}\n", .{cpu_info.cores});
+            try stdout.print("  └─ No. of threads: {?d}\n", .{cpu_info.threads});
             try stdout.print("Memory\n", .{});
-            try stdout.print(" └─ Total: {?d} KiB\n", .{memory_info.physical_total});
-            try stdout.print("    └─ Free: {?d} KiB\n", .{memory_info.physical_free});
+            try stdout.print("└─Total: {?d} KiB\n", .{memory_info.physical_total});
+            try stdout.print("  └─ Free: {?d} KiB\n", .{memory_info.physical_free});
+            try stdout.print("OS\n", .{});
+            try stdout.print("├─Name: {?s}\n", .{os_info.name});
+            try stdout.print("└─Version: {?s}\n", .{os_info.version});
         },
     }
 }
--- src/os.zig
+++ src/os.zig
@@ -1,18 +1,65 @@
 const std = @import("std");
-const containers = @import("containers");
+const ASCIIString = @import("containers").ASCIIString;
 
 pub const OSInfo = struct {
     const Self = @This();
 
     allocator: std.mem.Allocator,
+    name: ?ASCIIString,
+    version: ?ASCIIString,
 
     pub fn init(allocator: std.mem.Allocator) Self {
         return Self{
             .allocator = allocator,
+            .name = null,
+            .version = null,
         };
     }
 
     pub fn deinit(self: *Self) void {
-        _ = self;
+        if (self.name) |*name| {
+            name.deinit();
+        }
+
+        if (self.version) |*version| {
+            version.deinit();
+        }
+    }
+
+    pub fn fetch(self: *Self) !void {
+        const version_file = try std.fs.openFileAbsolute("/proc/version", .{ .mode = .read_only });
+        defer version_file.close();
+
+        var version_string = blk: {
+            const reader = version_file.reader();
+
+            const buffer = try reader.readAllAlloc(self.allocator, 2048);
+            defer self.allocator.free(buffer);
+
+            var string = ASCIIString.init(self.allocator);
+
+            try string.pushString(buffer[0 .. buffer.len - 1]);
+
+            break :blk string;
+        };
+
+        try version_string.removeString(0, 14);
+
+        const index = version_string.indexOf(" ").?;
+
+        try version_string.removeString(index, 2048);
+
+        if (self.name) |*name| {
+            name.deinit();
+        }
+
+        if (self.version) |*version| {
+            version.deinit();
+        }
+
+        self.name = ASCIIString.init(self.allocator);
+        try self.name.?.pushString("Linux");
+
+        self.version = version_string;
     }
 };
